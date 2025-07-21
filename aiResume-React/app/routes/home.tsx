0af1d56da1d88cde62e230892c036ada9da3a39c
himanshu8503 <himanshudiwakar8503@gmail.com>
2025-07-21 23:25:50 +0530

    adding nav in my first page

--- aiResume-React/app/routes/home.tsx
+++ aiResume-React/app/routes/home.tsx
@@ -1,13 +1,24 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Help you to track your resume to make ready for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+      </div>
+    </section>
+
+    {[]}
+  </main>;
 }
