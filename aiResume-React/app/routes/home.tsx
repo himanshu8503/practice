b78338f7c5d4779735a8b60d249038125ed8e4b4
himanshu8503 <himanshudiwakar8503@gmail.com>
2025-07-23 20:15:22 +0530

    creating home resume display

--- aiResume-React/app/routes/home.tsx
+++ aiResume-React/app/routes/home.tsx
@@ -1,24 +1,38 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-
+import { resumes } from "~/Constents";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Resumind" },
-    { name: "description", content: "Help you to track your resume to make ready for your dream job!" },
+    {
+      name: "description",
+      content:
+        "Help you to track your resume to make ready for your dream job!",
+    },
   ];
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    <Navbar/>
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
 
-    <section className="main-section">
-      <div className="page-heading">
-        <h1>Track Your Applications & Resume Ratings</h1>
-      </div>
-    </section>
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
 
-    {[]}
-  </main>;
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
